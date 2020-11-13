71dc843a1f4e782e39b5c69c15d3202d78d79fb6
ruhollah_jafari <jruhollah@gmail.com>
2020-11-13 16:56:00 +0330

    اضافه کردن کانکشن استرینگ

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,9 +4,28 @@ namespace ConsoleApp
 {
     class Program
     {
+        private static SamuraiAppData.SamuraiContext context = new SamuraiAppData.SamuraiContext();//این روش خوب نیست 
+        //فقط برای برنامه های کوچیک خوبه => بهترین روش استفاده از الگوی ریپازیتوری و یونیت آو ورک است 
+        //در این پروژه هدف صرفا EF CORE 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                context.Database.EnsureCreated();
+            GetSamurais("Before Add : ");
+            AddSamurai();
+            GetSamurais("After Add : ");
+            Console.WriteLine("Press Any Key ...");
+            Console.ReadLine();
+        }
+        private static void AddSamurai()
+        {
+            var samurai = new SamuraiAppDomain.Samurai { Name = "Ruholah Jafari" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+            
+        }
+        private static void GetSamurais(string text)
+        {
+            var samurais =context.Samurais
         }
     }
 }
--- SamuraiAppData/SamuraiContext.cs
+++ SamuraiAppData/SamuraiContext.cs
@@ -7,5 +7,10 @@ namespace SamuraiAppData
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> quotes { get; set; }
         public DbSet<Clan> clans { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //اضافه کردن کانکشن استرینگ 
+        {
+            optionsBuilder.UseSqlServer(
+            "Data Source=GEEK;Initial Catalog=SamuraiAppData;Integrated Security=True");
+                }
     }
 }
