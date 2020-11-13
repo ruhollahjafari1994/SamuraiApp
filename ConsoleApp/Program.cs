653a29de2951e512a1d26a4da38c9f52a025b15c
ruhollah_jafari <jruhollah@gmail.com>
2020-11-13 17:32:13 +0330

    This IS Not Best Practise For Real Application Just The Very Quickly Experience The Persistence Before Going Further with Your Learning

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,8 @@ namespace ConsoleApp
     {
         private static SamuraiAppData.SamuraiContext context = new SamuraiAppData.SamuraiContext();//این روش خوب نیست 
         //فقط برای برنامه های کوچیک خوبه => بهترین روش استفاده از الگوی ریپازیتوری و یونیت آو ورک است 
-        //در این پروژه هدف صرفا EF CORE 
+        //در این پروژه هدف صرفا EF CORE => صرفا سریع ترین تجربه کار با EF Core هست
+        //This IS Not Best Practise For Real Application Just The Very Quickly Experience The Persistence Before Going Further with Your Learning 
         static void Main(string[] args)
         {
                 context.Database.EnsureCreated();
@@ -25,7 +27,12 @@ namespace ConsoleApp
         }
         private static void GetSamurais(string text)
         {
-            var samurais =context.Samurais
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text} : Samurai Count Is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
--- SamuraiAppData/SamuraiContext.cs
+++ SamuraiAppData/SamuraiContext.cs
@@ -5,6 +5,7 @@ namespace SamuraiAppData
     public class SamuraiContext :DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
+
         public DbSet<Quote> quotes { get; set; }
         public DbSet<Clan> clans { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //اضافه کردن کانکشن استرینگ 
